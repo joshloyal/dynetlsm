e4af3adace78fc29bccc2927696c754daf2ce053
Joshua Loyal <joshua.d.loyal@gmail.com>
2022-02-05 16:11:48 -0600

    updated simulations

--- examples/homogeneous_simulation.py
+++ examples/homogeneous_simulation.py
@@ -3,9 +3,9 @@ Runs the time-homogeneous simulations found in the
 paper 'A Bayesian nonparametric latent space approach to modeling evolving
 communities in dynamic networks' by Joshua Loyal and Yuguo Chen
 """
-
 import glob
 import os
+import plac
 
 import pandas as pd
 import numpy as np
@@ -13,12 +13,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.utils import check_random_state
-from sklearn.metrics import adjusted_rand_score
+from sklearn.metrics import adjusted_rand_score, roc_auc_score
 
-from dynetlsm import DynamicNetworkHDPLPCM
+from dynetlsm import DynamicNetworkHDPLPCM, DynamicNetworkLPCM
 from dynetlsm.datasets import synthetic_static_community_dynamic_network
+from dynetlsm.datasets import homogeneous_simulation
 from dynetlsm.model_selection.approx_bic import calculate_cluster_counts
-from dynetlsm.metrics import variation_of_information
+from dynetlsm.model_selection import minimize_posterior_expected_vi
+from dynetlsm.model_selection import train_test_split
+from dynetlsm.metrics import variation_of_information, out_of_sample_auc
 from dynetlsm.network_statistics import density, modularity
 
 
@@ -43,77 +46,121 @@ def posterior_per_time_step(model):
 
 def benchmark_single(n_iter=10000, burn=5000, tune=1000,
                      outfile_name='benchmark',
+                     model_type='hdp_lpcm',
+                     selection_type='map',
+                     sim_type='hard',
                      random_state=None):
     random_state = check_random_state(random_state)
 
-    # generate simulated networks
-    Y, X, z, intercept, _, _ = synthetic_static_community_dynamic_network(
-        n_time_steps=6, n_nodes=120, random_state=random_state)
+    Y, X, z, intercept, _, _, _, _ = homogeneous_simulation(
+        n_time_steps=7, n_nodes=120,
+        simulation_type=sim_type,
+        random_state=random_state)
+
+    Y, Y_ahead = Y[:6], Y[6]
+    Y_obs, test_indices = train_test_split(
+        Y, test_size=0.1, random_state=random_state)
 
     # fit HDP-LPCM
-    model = DynamicNetworkHDPLPCM(n_iter=n_iter,
-                                  burn=burn,
-                                  tune=tune,
-                                  tune_interval=1000,
-                                  is_directed=False,
-                                  selection_type='vi',
-                                  n_components=10,
-                                  random_state=random_state).fit(Y)
+    if model_type == 'hdp_lpcm':
+        model = DynamicNetworkHDPLPCM(n_iter=n_iter,
+                                      burn=burn,
+                                      tune=tune,
+                                      tune_interval=1000,
+                                      is_directed=False,
+                                      selection_type='vi',
+                                      n_components=10,
+                                      random_state=random_state).fit(Y_obs)
+    else:
+        model = DynamicNetworkLPCM(n_iter=n_iter,
+                                   burn=burn,
+                                   tune=tune,
+                                   tune_interval=1000,
+                                   is_directed=False,
+                                   selection_type=selection_type,
+                                   n_components=6,
+                                   random_state=random_state).fit(Y_obs)
 
     # MAP: number of clusters per time point
     map_counts = counts_per_time_step(model.z_)
 
     # Posterior group count probabilities
-    probas = posterior_per_time_step(model)
+    if model_type == 'hdp_lpcm':
+        probas = posterior_per_time_step(model)
+        results = pd.DataFrame(probas)
+    else:
+        results = pd.DataFrame()
 
     # create dataframe of results
-    results = pd.DataFrame(probas)
     results['map_counts'] = map_counts
 
     # goodness-of-fit metrics for MAP
     results['insample_auc'] = model.auc_
-    results['vi'] = variation_of_information(z.ravel(), model.z_.ravel())
+    results['outsample_auc'] = out_of_sample_auc(
+        Y, model.missings_, test_indices)
 
-    # time average VI
+    # one-step ahead predictions
+    indices = np.tril_indices(Y.shape[1], k=-1)
+
+    pred_probas = model.forecast_probas_marginalized_[indices]
+    results['onestep_auc'] = roc_auc_score(
+        Y_ahead[indices], pred_probas)
+
+    # Variation of Information
+    results['vi'] = variation_of_information(
+        z[:6].ravel(), model.z_[:6].ravel())
     vi = 0.
     for t in range(Y.shape[0]):
-        vi += variation_of_information(z[t], model.z_[t])
+        vi_t = variation_of_information(z[t], model.z_[t])
+        results['vi_{}'.format(t)] = vi_t
+        vi += vi_t
     results['vi_avg'] = vi / Y.shape[0]
 
-    results['rand_index'] = adjusted_rand_score(z.ravel(), model.z_.ravel())
 
-    # time average rand
+    # adjusted rand index
+    results['rand_index'] = adjusted_rand_score(
+        z[:6].ravel(), model.z_[:6].ravel())
     adj_rand = 0.
     for t in range(Y.shape[0]):
-        adj_rand += adjusted_rand_score(z[t], model.z_[t])
+        adj_t = adjusted_rand_score(z[t], model.z_[t])
+        results['rand_{}'.format(t)] = adj_t
+        adj_rand += adj_t
     results['rand_avg'] = adj_rand / Y.shape[0]
-
-    # info about simulated networks
-    results['modularity'] = modularity(Y, z)
-    results['density'] = density(Y)
 
     results.to_csv(outfile_name, index=False)
 
 
-# NOTE: This is meant to be run in parallel on a computer cluster!
+# NOTE: This is meant to be run in parallel on a computer cluster
 n_reps = 50
 out_dir = 'results'
+model_type = 'hdp_lpcm'
+selection_type = 'vi'
+sim_type = 'easy'
 
 # create a directory to store the results
 if not os.path.exists('results'):
     os.mkdir(out_dir)
 
 for i in range(n_reps):
-    benchmark_single(n_iter=35000, burn=10000, tune=5000, random_state=i,
-                     outfile_name=os.path.join(
-                        out_dir, 'benchmark_{}.csv'.format(i)))
+    benchmark_single(
+        n_iter=35000, burn=10000, tune=5000, random_state=i,
+        model_type=model_type, selection_type=selection_type,
+        sim_type=sim_type,
+        outfile_name=os.path.join(
+            out_dir, 'benchmark_{}.csv'.format(i)))
+
 
 # calculate median metric values
 n_time_steps = 6
-n_groups = 10
+
+if model_type == 'lpcm':
+    n_groups = 6
+else:
+    n_groups = 10
 
 n_files = len(glob.glob('results/*'))
-stat_names = ['insample_auc', 'vi_avg', 'rand_avg']
+stat_names = ['insample_auc', 'outsample_auc', 'onestep_auc', 'vi',
+              'rand_index', 'vi_avg', 'rand_avg']
 data = np.zeros((n_files, len(stat_names)))
 for i, file_name in enumerate(glob.glob('results/*')):
     df = pd.read_csv(file_name)
@@ -157,7 +204,10 @@ data = np.zeros((n_time_steps, n_groups), dtype=np.int)
 for sim_id, file_name in enumerate(glob.glob('results/*')):
     df = pd.read_csv(file_name)
     for t in range(n_time_steps):
-        data[t, df.iloc[t, n_groups + 1] - 1] +=1
+        if model_type == 'lpcm':
+            data[t, df.iloc[t, 0] - 1] +=1
+        else:
+            data[t, df.iloc[t, n_groups + 1] - 1] +=1
 
 data = pd.DataFrame(data, columns=range(1, n_groups + 1), index=range(1, n_time_steps + 1))
 mask = data.values == 0
--- examples/inhomogeneous_simulation.py
+++ examples/inhomogeneous_simulation.py
@@ -3,9 +3,9 @@ Runs the time-inhomogeneous simulations found in the
 paper 'A Bayesian nonparametric latent space approach to modeling evolving
 communities in dynamic networks' by Joshua Loyal and Yuguo Chen
 """
-
 import glob
 import os
+import plac
 
 import pandas as pd
 import numpy as np
@@ -13,12 +13,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.utils import check_random_state
-from sklearn.metrics import adjusted_rand_score
+from sklearn.metrics import adjusted_rand_score, roc_auc_score
 
-from dynetlsm import DynamicNetworkHDPLPCM
-from dynetlsm.datasets import synthetic_dynamic_network
+from dynetlsm import DynamicNetworkHDPLPCM, DynamicNetworkLPCM
+from dynetlsm.datasets import inhomogeneous_simulation
 from dynetlsm.model_selection.approx_bic import calculate_cluster_counts
-from dynetlsm.metrics import variation_of_information
+from dynetlsm.model_selection import minimize_posterior_expected_vi
+from dynetlsm.model_selection import train_test_split
+from dynetlsm.metrics import variation_of_information, out_of_sample_auc
 from dynetlsm.network_statistics import density, modularity
 
 
@@ -44,14 +46,16 @@ def posterior_per_time_step(model):
 
 def benchmark_single(n_iter=10000, burn=5000, tune=1000,
                      outfile_name='benchmark',
+                     sim_type='hard',
                      random_state=None):
     random_state = check_random_state(random_state)
 
-    # generate simulated networks
-    Y, X, z, intercept, _, _ = synthetic_dynamic_network(
-        n_time_steps=9, n_nodes=120, is_directed=False,
-        lmbda=0.8, intercept=1.0, sticky_const=20,
-        random_state=random_state)
+    Y, X, z, intercept, _, _, _, _ = inhomogeneous_simulation(
+        n_nodes=120, random_state=random_state, simulation_type=sim_type)
+
+    Y, Y_ahead = Y[:9], Y[9]
+    Y_obs, test_indices = train_test_split(
+        Y, test_size=0.1, random_state=random_state)
 
     # fit HDP-LPCM
     model = DynamicNetworkHDPLPCM(n_iter=n_iter,
@@ -59,80 +63,79 @@ def benchmark_single(n_iter=10000, burn=5000, tune=1000,
                                   tune=tune,
                                   tune_interval=1000,
                                   is_directed=False,
-                                  n_components=10,
                                   selection_type='vi',
-                                  random_state=random_state).fit(Y)
+                                  n_components=10,
+                                  random_state=random_state).fit(Y_obs)
 
     # MAP: number of clusters per time point
     map_counts = counts_per_time_step(model.z_)
 
     # Posterior group count probabilities
     probas = posterior_per_time_step(model)
+    results = pd.DataFrame(probas)
 
     # create dataframe of results
-    results = pd.DataFrame(probas)
     results['map_counts'] = map_counts
 
     # goodness-of-fit metrics for MAP
     results['insample_auc'] = model.auc_
+    results['outsample_auc'] = out_of_sample_auc(
+        Y, model.missings_, test_indices)
 
-    # clustering results
-    results['vi'] = variation_of_information(z.ravel(), model.z_.ravel())
-    results['vi_t1'] = variation_of_information(z[:3].ravel(), model.z_[:3].ravel())
-    results['vi_t2'] = variation_of_information(z[3:6].ravel(), model.z_[3:6].ravel())
-    results['vi_t3'] = variation_of_information(z[6:].ravel(), model.z_[6:].ravel())
+    indices = np.tril_indices(Y.shape[1], k=-1)
 
-    # time average VI
+    pred_probas = model.forecast_probas_marginalized_[indices]
+    results['onestep_auc'] = roc_auc_score(
+        Y_ahead[indices], pred_probas)
+
+    # Variation of Information
+    results['vi'] = variation_of_information(
+        z[:9].ravel(), model.z_.ravel())
     vi = 0.
     for t in range(Y.shape[0]):
-        vi += variation_of_information(z[t], model.z_[t])
+        vi_t = variation_of_information(z[t], model.z_[t])
+        results['vi_{}'.format(t)] = vi_t
+        vi += vi_t
     results['vi_avg'] = vi / Y.shape[0]
 
-    results['rand_index'] = adjusted_rand_score(z.ravel(), model.z_.ravel())
-    results['rand_t1'] = adjusted_rand_score(z[:3].ravel(), model.z_[:3].ravel())
-    results['rand_t2'] = adjusted_rand_score(z[3:6].ravel(), model.z_[3:6].ravel())
-    results['rand_t3'] = adjusted_rand_score(z[6:].ravel(), model.z_[6:].ravel())
-
-    # time average rand
+    # adjusted rand index
+    results['rand_index'] = adjusted_rand_score(
+        z[:9].ravel(), model.z_.ravel())
     adj_rand = 0.
     for t in range(Y.shape[0]):
-        adj_rand += adjusted_rand_score(z[t], model.z_[t])
+        adj_t = adjusted_rand_score(z[t], model.z_[t])
+        results['rand_{}'.format(t)] = adj_t
+        adj_rand += adj_t
     results['rand_avg'] = adj_rand / Y.shape[0]
 
-    # info about simulated networks
-    results['modularity'] = modularity(Y, z)
-    results['modularity_t1'] = modularity(Y[:3], z[:3])
-    results['modularity_t2'] = modularity(Y[3:6], z[3:6])
-    results['modularity_t3'] = modularity(Y[6:], z[6:])
-
-    results['density'] = density(Y)
-    results['density_t1'] = density(Y[:3])
-    results['density_t2'] = density(Y[3:6])
-    results['density_t3'] = density(Y[6:])
-
-    results.to_csv(outfile_name + '.csv', index=False)
+    results.to_csv(outfile_name, index=False)
 
 
-# NOTE: This is meant to be run in parallel on a computer cluster!
+# NOTE: This is meant to be run in parallel on a computer cluster
 n_reps = 50
+sim_type = 'easy'
 out_dir = 'results'
 
 # create a directory to store the results
-if not os.path.exists('results'):
+if not os.path.exists(out_dir):
     os.mkdir(out_dir)
 
+
 for i in range(n_reps):
-    benchmark_single(n_iter=35000, burn=10000, tune=5000, random_state=i,
-                     outfile_name=os.path.join(
-                        out_dir, 'benchmark_{}.csv'.format(i)))
+    benchmark_single(
+        n_iter=35000, burn=10000, tune=5000, random_state=i,
+        sim_type=sim_type,
+        outfile_name=os.path.join(
+            out_dir, 'benchmark_{}.csv'.format(i)))
+
 
 # calculate median metric values
 n_time_steps = 9
 n_groups = 10
 
 n_files = len(glob.glob('results/*'))
-stat_names = ['insample_auc', 'vi_avg', 'vi_t1', 'vi_t2', 'vi_t3',
-              'rand_avg', 'rand_t1', 'rand_t2', 'rand_t3']
+stat_names = ['insample_auc', 'outsample_auc', 'onestep_auc', 'vi',
+              'rand_index', 'vi_avg', 'rand_avg']
 data = np.zeros((n_files, len(stat_names)))
 for i, file_name in enumerate(glob.glob('results/*')):
     df = pd.read_csv(file_name)
@@ -143,25 +146,6 @@ print('Median Metrics:')
 print(data.median(axis=0))
 print('Metrics SD:')
 print(data.std(axis=0))
-
-data = data[['vi_avg', 'vi_t1', 'vi_t2', 'vi_t3',
-             'rand_avg', 'rand_t1', 'rand_t2', 'rand_t3']]
-data.columns = ['Avg. VI', 'VI (t = 1 - 3)', 'VI (t = 4 - 6)', 'VI (t = 7 - 9)',
-                'Avg. ARI', 'ARI (t = 1 - 3)', 'ARI (t = 4 - 6)',
-                'ARI (t = 7 - 9)']
-
-# boxplots of the metrics
-plt.rc('font', family='sans-serif', size=24)
-fig, ax = plt.subplots(figsize=(10, 6))
-g = sns.boxplot(x='variable', y='value',
-                data=pd.melt(data), fliersize=2.0, ax=ax)
-g.tick_params(labelsize=8)
-sns.despine()
-ax.set(xlabel='', ylabel='')
-plt.savefig('performance_plot.png', dpi=300)
-
-# clear figure
-plt.clf()
 
 # plot posterior boxplots
 data = {'probas': [], 'cluster_number': [], 't': []}
@@ -197,8 +181,7 @@ for sim_id, file_name in enumerate(glob.glob('results/*')):
     for t in range(n_time_steps):
         data[t, df.iloc[t, n_groups + 1] - 1] +=1
 
-data = pd.DataFrame(data,
-    columns=range(1, n_groups + 1), index=range(1, n_time_steps + 1))
+data = pd.DataFrame(data, columns=range(1, n_groups + 1), index=range(1, n_time_steps + 1))
 mask = data.values == 0
 
 g = sns.heatmap(data, annot=True, cmap="Blues", cbar=False, mask=mask)
